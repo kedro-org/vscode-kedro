bc092375753cb74186d110a6845e26c8622406fa
Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>
2024-08-28 15:47:34 +0100

    Added pydantic in python script
    Signed-off-by: Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>

--- bundled/tool/install_dependencies.py
+++ bundled/tool/install_dependencies.py
@@ -1,7 +1,5 @@
 import subprocess
 import sys
-import shutil
-import glob
 from pathlib import Path
 
 def install_dependencies(libsPath):
@@ -9,7 +7,21 @@ def install_dependencies(libsPath):
         import fastapi
         import orjson
     except ImportError:
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'fastapi>=0.100.0,<0.200.0', 'orjson>=3.9, <4.0', '-t', Path(libsPath), '--no-cache-dir'])
+        subprocess.check_call(
+            [
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                "fastapi>=0.100.0,<0.200.0",
+                "pydantic>=2.0.0", # In case of FastAPI installs pydantic==1
+                "orjson>=3.9, <4.0",
+                "-t",
+                Path(libsPath),
+                "--no-cache-dir",
+            ]
+        )
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:

199ef491311412bf7494948324d50d09c301db97
Nok Lam Chan <nok.lam.chan@quantumblack.com>
2024-09-12 17:31:18 +0100

    Update schema
    Signed-off-by: Nok Lam Chan <nok.lam.chan@quantumblack.com>

--- src/common/telemetry.ts
+++ src/common/telemetry.ts
@@ -13,12 +13,21 @@ const HEAP_APPID_PROD = '4039408868'; // todo: Dev server, change it back to pro
 const HEAP_ENDPOINT = 'https://heapanalytics.com/api/track';
 const HEAP_HEADERS = { 'Content-Type': 'application/json' };
 
-export async function sendHeapEvent(eventName: string, properties?: any, identity?: string): Promise<void> {
+export async function sendHeapEvent(commandName: string, properties?: any, identity?: string): Promise<void> {
+    var telemetryData;
+    const eventName = "Kedro VSCode Command";
+
+    if (properties) {
+        telemetryData = { ...properties };
+        telemetryData["command_name"] = eventName;
+    }
+
+
     const data: HeapData = {
         app_id: HEAP_APPID_PROD,
         event: eventName,
         timestamp: new Date().toISOString(),
-        properties: properties || {},
+        properties: telemetryData || {},
     };
 
     if (identity) {

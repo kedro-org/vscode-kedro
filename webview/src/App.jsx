a5986a769821fb527a5ea127b41001b9e726cf80
Jitendra Gundaniya <38945204+jitu5@users.noreply.github.com>
2025-03-19 17:56:58 +0000

    Update Kedro-Viz to 11.0.0 as minimum requirement for extension (#176)
    * Update Kedro-Viz to 11.0.0 as minimum requirement
    
    Signed-off-by: Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>
    
    * Update App.jsx
    
    ---------
    
    Signed-off-by: Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>

--- webview/src/App.jsx
+++ webview/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const toolbarOptions = {
     labelBtn: true,
     layerBtn: true,
-    expandPipelinesBtn: false,
+    expandPipelinesBtn: true,
     exportBtn: false,
     filterBtn: true,
   };
